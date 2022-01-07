470664be3a34082912be72581d86973564f9c747
icorvus <business.wrona@gmail.com>
2022-01-07 22:51:41 +0100

    Replaced e.keyCode for e.code as it turns out is better practice

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,12 +1,12 @@
 const keys = document.querySelectorAll('.key');
 keys.forEach((key) => {
   document.addEventListener('keydown', e => {
-    if (e.keyCode == key.getAttribute('data-key')) {
+    if (e.code == key.getAttribute('data-key')) {
       key.classList.toggle('playing');
     };
   });
   document.addEventListener('keyup', e => {
-    if (e.keyCode == key.getAttribute('data-key')) {
+    if (e.code == key.getAttribute('data-key')) {
       key.classList.toggle('playing');
     };
   });
@@ -15,7 +15,7 @@ keys.forEach((key) => {
 const sounds = document.querySelectorAll('audio');
 sounds.forEach((sound) => {
   document.addEventListener('keydown', e => {
-    if (e.keyCode == sound.getAttribute('data-key')) {
+    if (e.code == sound.getAttribute('data-key')) {
       sound.play();
     };
   });
